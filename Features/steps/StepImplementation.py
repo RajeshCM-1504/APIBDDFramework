c3f1e167a6043471bd76aa6e069a61ace330397f
RajeshCM-1504 <cmrajesh1504@gmail.com>
2025-07-07 17:01:10 +0530

    Update StepImplementation.py
    allure.attach updation

--- Features/steps/StepImplementation.py
+++ Features/steps/StepImplementation.py
@@ -20,13 +20,13 @@ def step_apicall(context):
         elapsed_time = time.time() - start_time
 
         assert context.response.status_code == 200, "❌ Status code is not 200"
-        print("✅ Status code is 200")
+        allure.attach("✅ Status code is 200")
 
         assert elapsed_time < 60, "❌ API took too long to respond"
-        print("✅ API took less than a minute to respond")
+        allure.attach("✅ API took less than a minute to respond")
 
     except requests.exceptions.Timeout:
-        print("❌ Request timed out!")
+        allure.attach("❌ Request timed out!")
 
 @then('competations list is displayed')
 def step_verification(context):
@@ -37,30 +37,30 @@ def step_verification(context):
     if competitions:
         competitionId = competitions[0].get("competitionId")
         assert competitionId.startswith("cpl::Football_Competition::"), f"❌ Unexpected competitionId format: {competitionId}"
-        print("✅ competitionId Validation is Passed")
+        allure.attach("✅ competitionId Validation is Passed")
 
         providerId = competitions[0].get("providerId")
         assert providerId.startswith("opta:Competition:"), f"❌ Unexpected competitionId format: {providerId}"
-        print("✅ providerId Validation is Passed")
+        allure.attach("✅ providerId Validation is Passed")
 
         name = competitions[0].get("name")
         assert isinstance(name, str), f"❌ comp_id is not a string: {type(name)}"
-        print("✅ name Validation is Passed")
+        allure.attach("✅ name Validation is Passed")
 
         officialName = competitions[0].get("officialName")
         assert isinstance(officialName, str), f"❌ comp_id is not a string: {type(officialName)}"
-        print("✅ officialName Validation is Passed")
+        allure.attach("✅ officialName Validation is Passed")
 
         shortName = competitions[0].get("shortName")
         assert isinstance(shortName, str), f"❌ comp_id is not a string: {type(shortName)}"
-        print("✅ officialName Validation is Passed")
+        allure.attach("✅ officialName Validation is Passed")
 
         acronymName = competitions[0].get("acronymName")
         assert isinstance(acronymName, str), f"❌ comp_id is not a string: {type(acronymName)}"
-        print("✅ acronymName Validation is Passed")
+        allure.attach("✅ acronymName Validation is Passed")
 
     else:
-        print("❌ No competitions found in the response.")
+        allure.attach("❌ No competitions found in the response.")
 
     schema = {
         "$schema": "http://json-schema.org/draft-07/schema#",
@@ -113,9 +113,9 @@ def step_verification(context):
 
     try:
         validate(instance=context.response.json(), schema=schema)
-        print("✅ Schema validation is Passed")
+        allure.attach("✅ Schema validation is Passed")
     except ValidationError as e:
-        print(f"❌ Schema validation failed: {e.message}")
+        allure.attach(f"❌ Schema validation failed: {e.message}")
 
 
 
